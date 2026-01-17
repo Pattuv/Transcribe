3dd720946b23bc726714bb3df4a02b98d87e7c34
Pattuv <161959486+Pattuv@users.noreply.github.com>
2026-01-17 14:06:21 -0500

    boilerplate code, project init

---
+++ extension/extension.js
@@ -0,0 +1,40 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    "Welcome to Transcribe. Use the Copy File Structure button to paste your file structure into your favorite AI tools and get building.",
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand(
+    "transcribe.copyStructure",
+    function () {
+      // The code you place here will be executed every time your command is executed
+      vscode.window.showInformationMessage("File Structure Copied!");
+    },
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
